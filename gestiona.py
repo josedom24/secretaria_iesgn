2c224a534f6b22921ad2f174bc24b0bb0cba90d5
Jose Domingo <jose@toshibita>
2016-09-27 15:04:42 +0200

    Empezamos...

---
+++ gestiona.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from bottle import template,request
+import sesion
+
+def my_template(name,info={}):
+	
+	info["login"]=sesion.get("user") if sesion.islogin() else ""
+	
+
+	return template(name,info=info)
+
