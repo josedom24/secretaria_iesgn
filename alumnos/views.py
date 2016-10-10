af6e64bbd3b2f27bf7cf5be978c88d28da94c692
José Domingo Muñoz <josedom24@gmail.com>
2016-10-10 09:03:10 +0200

    Cambio estrucura

--- alumnos/views.py
+++ alumnos/views.py
@@ -1,9 +1,15 @@
 from bottle import app, route, template, run, static_file, error,request,response,redirect,error
+import bottle
+import os
 import sesion
 import hashlib
 from gestiona import *
 from beaker.middleware import SessionMiddleware
 from model import *
+
+base_path = os.path.abspath(os.path.dirname(__file__))
+alumnos_path = os.path.join(base_path, 'templates')
+bottle.TEMPLATE_PATH.insert(0, alumnos_path)
 
 @route('/alumnos',method=['get','post'])
 def alumnos():
@@ -13,6 +19,7 @@ def alumnos():
         info["params"]={"curso":curso}
         info["alumnos"]=Alumno.select().where(Alumno.Unidad==curso)
         info["cursos"]=Curso.select()    
+        print bottle.TEMPLATE_PATH
         
         return my_template('alumnos.tpl',info=info)
     else:
